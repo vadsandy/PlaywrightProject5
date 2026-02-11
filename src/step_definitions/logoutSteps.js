f699ce442145a2a48bcbecc3f2750ec2710c90be
vadsandy <sandeep.dct@gmail.com>
2026-02-11 13:22:10 +0530

    new changes

--- src/step_definitions/logoutSteps.js
+++ src/step_definitions/logoutSteps.js
@@ -4,24 +4,24 @@ const {DataReader} = require('../utils/DataReader');
 const {LoginPage} = require('../pages/LoginPage');
 const {ProfilePage} = require('../pages/ProfilePage');
 
-Given ('I enter username {string} and password {string} on login page and click login button', async function(){
+Given (/^I enter username "([^"]*)" and password "([^"]*)" on login page and click login button$/, async function(){
     this.loginPage = new LoginPage(this.page);
     this.loginPage.gotoLoginPageAndLogin(user, password);
 });
         
 
-When ('I click on the Logout button', async function (){
+When (/^I click on the Logout button$/, async function (){
     this.profilePage = new ProfilePage(this.page);
     this.profilePage.logout();
 });
 
         
-Then ('I should be redirected to the login page', async function() {
+Then (/^I should be redirected to the login page$/, async function() {
     await expect(this.page).toHaveURL(/login/);
 });
         
 
-Then ('I should see the "Login" button to confirm I am out', async function() {
+Then (/^I should see the Login button to confirm I am out$/, async function() {
     //this.loginPage = new LoginPage(this.page);
     await expect(this.loginPage.loginBtn).toContainText('Login');
 }); 
